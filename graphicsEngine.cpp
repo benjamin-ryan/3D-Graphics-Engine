a1d3145280862a7a759e887712d7db9fead2df7b
Benjamin <benjamin.ryan@comcast.net>
2024-08-07 16:37:08 -0400

    Rasterize triangles

--- graphicsEngine.cpp
+++ graphicsEngine.cpp
@@ -28,35 +28,6 @@ vertex normalize(const vertex& v) {
     return vertex{v.x / length, v.y / length, v.z / length};
 }
 
-void fillTriangle(SDL_Renderer* render, coord p1, coord p2, coord p3) {
-    // Sort the points by their y-coordinate (ascending)
-    if (p1.y > p2.y) std::swap(p1, p2);
-    if (p1.y > p3.y) std::swap(p1, p3);
-    if (p2.y > p3.y) std::swap(p2, p3);
-
-    // Function to interpolate x coordinate
-    auto interpolate = [](coord p1, coord p2, int y) {
-        if (p1.y == p2.y) return p1.x;
-        return p1.x + (p2.x - p1.x) * (y - p1.y) / (p2.y - p1.y);
-    };
-
-    // Draw the upper part of the triangle
-    for (int y = p1.y; y <= p2.y; ++y) {
-        int x1 = interpolate(p1, p3, y);
-        int x2 = interpolate(p1, p2, y);
-        if (x1 > x2) std::swap(x1, x2);
-        SDL_RenderDrawLine(render, x1, y, x2, y);
-    }
-
-    // Draw the lower part of the triangle
-    for (int y = p2.y; y <= p3.y; ++y) {
-        int x1 = interpolate(p1, p3, y);
-        int x2 = interpolate(p2, p3, y);
-        if (x1 > x2) std::swap(x1, x2);
-        SDL_RenderDrawLine(render, x1, y, x2, y);
-    }
-}
-
 Renderer::Renderer(SDL_Window *_window, SDL_Renderer *_render, mesh &_mesh)
 {
     SDL_GetWindowSize(_window, &windowWidth, &windowHeight);
@@ -88,9 +59,9 @@ void Renderer::renderFrame()
         vertex rotatedVertex2 = rotateX(rotateY(tri.v[1]));
         vertex rotatedVertex3 = rotateX(rotateY(tri.v[2]));
 
-        // coord edge1 = projection(tri.v[0]);
-        // coord edge2 = projection(tri.v[1]);
-        // coord edge3 = projection(tri.v[2]);
+        //coord edge1 = projection(tri.v[0]);
+        //coord edge2 = projection(tri.v[1]);
+        //coord edge3 = projection(tri.v[2]);
 
         // Calculate the normal vector of the triangle
         vertex e1 = subtractV(rotatedVertex2, rotatedVertex1);
@@ -107,15 +78,15 @@ void Renderer::renderFrame()
 
         if (dotProduct(normal, cameraRay) > 0)
         {
-        coord edge1 = projection(rotatedVertex1);
-        coord edge2 = projection(rotatedVertex2);
-        coord edge3 = projection(rotatedVertex3);
+            coord edge1 = projection(rotatedVertex1);
+            coord edge2 = projection(rotatedVertex2);
+            coord edge3 = projection(rotatedVertex3);
 
-        fillTriangle(render, edge1, edge2, edge3);
+            fillTriangle(render, edge1, edge2, edge3);
 
-        SDL_RenderDrawLine(render, edge1.x, edge1.y, edge2.x, edge2.y);
-        SDL_RenderDrawLine(render, edge2.x, edge2.y, edge3.x, edge3.y);
-        SDL_RenderDrawLine(render, edge3.x, edge3.y, edge1.x, edge1.y);
+            //SDL_RenderDrawLine(render, edge1.x, edge1.y, edge2.x, edge2.y);
+            //SDL_RenderDrawLine(render, edge2.x, edge2.y, edge3.x, edge3.y);
+            //SDL_RenderDrawLine(render, edge3.x, edge3.y, edge1.x, edge1.y);
         }
     }
     SDL_RenderPresent(render);
@@ -200,5 +171,24 @@ vertex Renderer::rotateY(vertex v)
 
 void Renderer::fillTriangle(SDL_Renderer *renderer, coord v1, coord v2, coord v3)
 {
-
+    SDL_Color tri_color={0,0,0,255};
+    SDL_Vertex vertices[3] = {
+        {
+            { v1.x,v1.y },
+            { 255, 255, 255, 0xFF }, // 255, 0, 0, 0xFF
+            { 0.f, 0.f }
+        },
+        {
+            { v2.x, v2.y },
+            { 255, 255, 255, 0xFF }, // 0, 255, 0, 0xFF
+            { 0.f, 0.f }
+        },
+        {
+            { v3.x, v3.y },
+            { 255, 255, 255, 0xFF }, // 0, 0, 255, 0xFF 
+            { 0.f, 0.f }
+        }
+    };
+  
+    SDL_RenderGeometry(renderer,NULL,vertices,3,NULL,0);
 }
