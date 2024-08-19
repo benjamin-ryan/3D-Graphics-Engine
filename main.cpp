4945660096c35caf85ffc06aa8084b95714e8994
Benjamin <benjamin.ryan@comcast.net>
2024-08-18 22:01:59 -0400

    Main

--- main.cpp
+++ main.cpp
@@ -55,8 +55,8 @@ int main()
     std::vector<mesh> models(2);
 
     Renderer frameRenderer(window, renderer, models);
-    //frameRenderer.loadObjFile("Models/cube.obj", 0);
-    frameRenderer.loadObjTextureFile("./Models/snorkelWithTextures.obj", "./Textures/snorkel.bmp", 0);
+    frameRenderer.loadObjFile("Models/cube.obj", 0);
+    //frameRenderer.loadObjTextureFile("./Models/snorkelWithTextures.obj", "./Textures/snorkel.bmp", 0);
     frameRenderer.setControlCamera(false);
 
     bool running = true;
